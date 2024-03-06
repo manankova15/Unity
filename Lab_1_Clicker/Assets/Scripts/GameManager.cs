d61d458f80995e4e2879456d9e8405c167056f60
Julia <yusmanankova_1@edu.hse.ru>
2024-03-06 21:25:52 +0300

    feat:visual editing

--- Lab_1_Clicker/Assets/Scripts/GameManager.cs
+++ Lab_1_Clicker/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ namespace Scripts
 
         void Start()
         {
-            resourceBank.ChangeResource(GameResource.Humans, 10000);
+            resourceBank.ChangeResource(GameResource.Humans, 10);
             resourceBank.ChangeResource(GameResource.Food, 5);
             resourceBank.ChangeResource(GameResource.Wood, 5);
         }
