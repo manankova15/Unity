04ed9ffa94995ca8fb369a9a0727db93bf172d21
Julia <yusmanankova_1@edu.hse.ru>
2024-03-25 17:35:30 +0300

    feat: ArrayUtils upgraded

--- Crafting Mechanic/Assets/Scripts/Utils/ArrayUtils.cs
+++ Crafting Mechanic/Assets/Scripts/Utils/ArrayUtils.cs
@@ -2,6 +2,14 @@ namespace Utils
 {
     public static class ArrayUtils
     {
-        
+        public static T[] InitializeArray<T>(int length, T defaultValue)
+        {
+            var array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = defaultValue;
+            }
+            return array;
+        }
     }
 }
